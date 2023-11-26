0ae7d97d84b7c2587e7736e7c191aaba663b6b88
chetansirohi <chetansirohi36@gmail.com>
2023-11-26 16:28:32 +0530

    'home-page-setup'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,30 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-import "./App.css";
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+      </Link>
 
-  return (
-    <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
+      <Link
+        to="/create-post"
+        className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+      >
+        Create
+      </Link>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  </BrowserRouter>
+);
 
 export default App;
