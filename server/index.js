b5664f7f6756d9b4e9a470292694ccd348450f66
chetansirohi <chetansirohi36@gmail.com>
2023-11-27 23:20:35 +0530

    'updatedapis'

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:5173/"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    }),
+);
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,38 +1,25 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
-
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
+import OpenAI from "openai";
 
 const router = express.Router();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-const openai = new OpenAIApi(configuration);
-
-router.route('/').get((req, res) => {
-    res.status(200).json({ message: 'Hello from DALL-E!' });
-});
-
-router.route('/').post(async (req, res) => {
+router.route("/").post(async (req, res) => {
     try {
         const { prompt } = req.body;
-
-        const aiResponse = await openai.createImage({
+        const aiResponse = await openai.images.generate({
+            model: "dall-e-3",
             prompt,
             n: 1,
-            size: '1024x1024',
-            response_format: 'b64_json',
+            size: "1024x1024",
+            response_format: "b64_json",
         });
-
-        const image = aiResponse.data.data[0].b64_json;
+        const image = aiResponse.data[0].b64_json;
         res.status(200).json({ photo: image });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    } catch (e) {
+        console.log(e);
     }
 });
-
-export default router;
